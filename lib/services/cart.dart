dc96face82ef952b40c489a0ea8d58ecbbda4696
EdisonP <133362311+edions@users.noreply.github.com>
2025-02-17 22:41:47 +0800

    save customer and total

--- lib/services/cart.dart
+++ lib/services/cart.dart
@@ -27,23 +27,36 @@ class CartService {
   Future<void> clearCart(String userId) async {
     final cartReference = products.doc(userId).collection("cart");
     final orderReference = FirebaseFirestore.instance.collection("orders");
+    final userData = await FirebaseFirestore.instance
+        .collection('users')
+        .doc(userId)
+        .collection('info')
+        .doc('data')
+        .get();
 
     final QuerySnapshot cartSnapshot = await cartReference.get();
 
     List<Map<String, dynamic>> cartItems = [];
+    double total = 0.0;
 
     for (QueryDocumentSnapshot doc in cartSnapshot.docs) {
       Map<String, dynamic> itemData = doc.data() as Map<String, dynamic>;
       cartItems.add(itemData);
+
+      double price = double.tryParse(itemData['price']) ?? 0.0;
+      total += price;
+
       await cartReference.doc(doc.id).delete();
     }
+    String customerName = userData['name'] ?? 'Unknown';
 
     await orderReference.add({
       'userId': userId,
+      'customer': customerName,
+      'total': total,
       'itemData': cartItems,
       'transactionTimestamp': FieldValue.serverTimestamp(),
       'status': 'Pending',
     });
-
   }
 }
